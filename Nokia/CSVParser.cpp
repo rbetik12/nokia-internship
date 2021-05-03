e6369135d51fa077a5d6603df9bbd3779313d14b
rbetik12 <mr.prikota@gmail.com>
2021-05-03 18:25:53 +0300

    Added CLI

--- Nokia/CSVParser.cpp
+++ Nokia/CSVParser.cpp
@@ -1,19 +1,179 @@
 #include "CSVParser.h"
+#include <fstream>
+#include <iostream>
 
 CSVParser::CSVParser() {}
 
-CSVParser::~CSVParser() {}
+CSVParser::~CSVParser() {
+    Clear();
+}
 
 std::vector<std::string> CSVParser::GetParsedTable() {
     return std::vector<std::string>();
 }
 
-void CSVParser::ParseFile(std::string& filename) {}
+void CSVParser::PrintTable() {
+    std::string rowDel = "\t";
+    std::cout << rowDel;
+    for (auto col : colNames) {
+        std::cout << col << rowDel;
+    }
+    std::cout << std::endl;
 
-void CSVParser::ParseCSVLines() {}
-
-int CSVParser::CalculateFormula(std::string formula) {
-    return 0;
+    for (int row = 0; row < rowNames.size(); row++) {
+        std::cout << rowNames[row] << rowDel;
+        for (int col = 0; col < colNames.size(); col++) {
+            std::string el = rowColMap[rowNames[row]][colNames[col]];
+            if (el[0] == '=') {
+                std::cout << CalculateFormula(el) << rowDel;
+            }
+            else {
+                std::cout << el << rowDel;
+            }
+        }
+        std::cout << std::endl;
+    }
 }
 
-void CSVParser::SaveResult(int** dest, int result) {}
+void CSVParser::ParseFile(std::string filename, std::string delimiter) {
+    Clear();
+    std::ifstream inputStream(filename);
+
+    for (std::string line; std::getline(inputStream, line);) {
+        rawLines.push_back(line);
+    }
+
+    ParseCSVLines(delimiter);
+}
+
+void CSVParser::ParseCSVLines(std::string delimiter) {
+    std::vector<std::string> splittedRow;
+    std::string currentRow;
+    uint64_t colNum;
+
+    colNames = Split(rawLines[0], delimiter);
+    colNames.erase(colNames.begin());
+    rawLines.erase(rawLines.begin());
+
+    for (auto row : rawLines) {
+        splittedRow = Split(row, delimiter);
+        currentRow = splittedRow[0];
+        rowNames.push_back(currentRow);
+        splittedRow.erase(splittedRow.begin());
+        colNum = 0;
+
+        for (auto value : splittedRow) {
+            if (value[0] == '=') {
+                formulas[value] = nullptr;
+            }
+            colRowSearchMap[colNames[colNum] + currentRow] = value;
+            rowColMap[currentRow][colNames[colNum]] = value;
+            colNum += 1;
+        }
+    }
+}
+
+int CSVParser::CalculateFormula(std::string formula) {
+    if (formulas[formula] != nullptr) {
+        return *formulas[formula];
+    }
+    std::string op = GetOp(formula);
+    int arg0, arg1, result;
+    formula.erase(formula.begin());
+    std::vector<std::string> args = Split(formula, op);
+
+    std::string arg0Str = colRowSearchMap[args[0]] == "" ? args[0] : colRowSearchMap[args[0]];
+    std::string arg1Str = colRowSearchMap[args[1]] == "" ? args[1] : colRowSearchMap[args[1]];
+
+    arg0 = CalcArg(arg0Str);
+    arg1 = CalcArg(arg1Str);
+    
+    switch (op[0]) {
+    case '-':
+        result = arg0 - arg1;
+        break;
+    case '+':
+        result = arg0 + arg1;
+        break;
+    case '/':
+        result = arg0 / arg1;
+        break;
+    case '*':
+        result = arg0 * arg1;
+        break;
+    }
+
+    formulas["=" + formula] = new int;
+    std::memcpy(formulas["=" + formula], &result, sizeof(result));
+    return result;
+}
+
+int CSVParser::CalcArg(std::string& argStr) {
+    int res;
+
+    if (argStr[0] == '=') {
+        if (formulas[argStr] == nullptr) {
+            res = CalculateFormula(argStr);
+            SaveResult(&formulas[argStr], res);
+        }
+        else {
+            res = *formulas[argStr];
+        }
+    }
+    else {
+        res = std::stoi(argStr);
+    }
+
+    return res;
+}
+
+void CSVParser::SaveResult(int** dest, int result) {
+    *dest = new int;
+    std::memcpy(*dest, &result, sizeof(result));
+}
+
+void CSVParser::Clear() {
+    colNames.clear();
+    rowNames.clear();
+    rawLines.clear();
+    colRowSearchMap.clear();
+    for (auto entry0 : rowColMap) {
+        entry0.second.clear();
+    }
+    rowColMap.clear();
+    for (auto formula : formulas) {
+        delete formula.second;
+    }
+    formulas.clear();
+}
+
+std::string CSVParser::GetOp(std::string formula) {
+    for (uint64_t i = 0; i < formula.size(); i++) {
+        switch (formula[i]) {
+        case '-':
+            return "-";
+        case '+':
+            return "+";
+        case '/':
+            return "/";
+        case '*':
+            return "*";
+        }
+    }
+}
+
+std::vector<std::string> CSVParser::Split(std::string str, std::string delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    uint64_t pos;
+
+    while ((pos = str.find(delimiter)) != std::string::npos) {
+        token = str.substr(0, pos);
+        tokens.push_back(token);
+        str.erase(0, pos + delimiter.length());
+    }
+
+    tokens.push_back(str);
+
+    return tokens;
+}
--- Nokia/CSVParser.h
+++ Nokia/CSVParser.h
@@ -8,12 +8,21 @@ public:
     ~CSVParser();
 
     std::vector<std::string> GetParsedTable();
+    void PrintTable();
+    void ParseFile(std::string filename, std::string delimiter);
 private:
-    void ParseFile(std::string& filename);
-    void ParseCSVLines();
+    static std::string GetOp(std::string formula);
+    static std::vector<std::string> Split(std::string str, std::string delimiter);
+    void ParseCSVLines(std::string delimiter);
     int CalculateFormula(std::string formula);
+    int CalcArg(std::string& argStr);
     void SaveResult(int** dest, int result);
-    std::vector<std::string> lines;
+    void Clear();
+
+private:
+    std::vector<std::string> colNames;
+    std::vector<std::string> rowNames;
+    std::vector<std::string> rawLines;
     std::unordered_map<std::string, std::string> colRowSearchMap;
     std::unordered_map<std::string, std::unordered_map<std::string, std::string>> rowColMap;
     std::unordered_map<std::string, int*> formulas;
